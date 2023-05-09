a136200ff720c246629214c0ba9fd0acb46e60a0
sonia-goyal <111214390+sonia-goyal@users.noreply.github.com>
2023-05-09 16:47:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+# %%writefile app.py
 import streamlit as st
+import gdown
 
 # Create a title and subtitle
 st.title("DLOPS Demo")
@@ -14,9 +16,14 @@ model_args = NERArgs()
 # model_args.num_train_epochs=20
 model_args.labels_list = ['O', 'B-geo', 'B-gpe', 'B-per', 'I-geo', 'B-org', 'I-org', 'B-tim', 'I-per', 'I-gpe', 'I-tim']
 # model_args.overwrite_output_dir = True
-model = NERModel(mtype,r"model/",args=model_args,use_cuda=False)
-print("model loaded successfully")
+url = "https://drive.google.com/drive/folders/1-6ZGGP1g6BpHwpGDQUS3SD8lAnkz-0gr?usp=share_link"
+output = "checkpoint-7803-epoch-9"
+gdown.download_folder(url, quiet=False)
 
+model = NERModel(mtype,output,args=model_args,use_cuda=False)
+#model = NERModel(mtype,r"/content/drive/MyDrive/Classroom/DLOps (CSP7030) 22-23 Sem II/checkpoint-7803-epoch-9",args=model_args,use_cuda=False)
+print("model loaded successfully")
+# st.cache
 
 # Check if the user has entered a name
 if text:
