d4876636b61416e787947b9ddd354a831cbdcf00
sonia-goyal <111214390+sonia-goyal@users.noreply.github.com>
2023-05-09 18:00:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # %%writefile app.py
 import streamlit as st
 import gdown
+from simpletransformers.ner import NERModel, NERArgs
 
 # Create a title and subtitle
 st.title("DLOPS Demo")
@@ -9,22 +10,22 @@ st.subheader("Enter the text to be predicted")
 # Create a text input box for the user to enter their name
 text = st.text_input("text")
 
-from simpletransformers.ner import NERModel, NERArgs
+#@st.cache_resource
+def load_model():
+  mtype='xlmroberta'
+  model_args = NERArgs()
+  model_args.labels_list = ['O', 'B-geo', 'B-gpe', 'B-per', 'I-geo', 'B-org', 'I-org', 'B-tim', 'I-per', 'I-gpe', 'I-tim']
+  url = "https://drive.google.com/drive/folders/1-6ZGGP1g6BpHwpGDQUS3SD8lAnkz-0gr?usp=share_link"
+  output = "checkpoint-7803-epoch-9"
+  gdown.download_folder(url, quiet=False)
 
-mtype='xlmroberta'
-model_args = NERArgs()
-# model_args.num_train_epochs=20
-model_args.labels_list = ['O', 'B-geo', 'B-gpe', 'B-per', 'I-geo', 'B-org', 'I-org', 'B-tim', 'I-per', 'I-gpe', 'I-tim']
-# model_args.overwrite_output_dir = True
-url = "https://drive.google.com/drive/folders/1-6ZGGP1g6BpHwpGDQUS3SD8lAnkz-0gr?usp=share_link"
-output = "checkpoint-7803-epoch-9"
-gdown.download_folder(url, quiet=False)
+  model = NERModel(mtype,output,args=model_args,use_cuda=False)
+  print("model loaded successfully")
 
-model = NERModel(mtype,output,args=model_args,use_cuda=False)
-#model = NERModel(mtype,r"/content/drive/MyDrive/Classroom/DLOps (CSP7030) 22-23 Sem II/checkpoint-7803-epoch-9",args=model_args,use_cuda=False)
-print("model loaded successfully")
-# st.cache
+  return model
+# @st.cache
 
+model = load_model()
 # Check if the user has entered a name
 if text:
     # Display a greeting with the user's name
@@ -32,4 +33,4 @@ if text:
     predictions, raw_outputs = model.predict(["I am travelling to India  and I work for Google", "I am a Microsoft employee"])
     print(predictions[0])
     st.write("entities:")
-    #st.write(predictions[0])
+    st.write(predictions[0])
